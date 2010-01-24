e4335aca4d6e92d87f02c438cccef31bcab13d42
Amir Barylko <amir@barylko.com>
2010-01-24 03:40:03 -0600

    added project implementation

--- src/com/maventhought/kanbanpro/server/agilezen/AgileZen.java
+++ src/com/maventhought/kanbanpro/server/agilezen/AgileZen.java
@@ -1,51 +1,63 @@
 package com.maventhought.kanbanpro.server.agilezen;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.methods.GetMethod;
-
 import com.maventhought.kanbanpro.client.Project;
+import com.maventhought.kanbanpro.client.Project2;
 
 public class AgileZen {
 
-	public Iterable<Project> getProjects() throws Exception {
+	public Iterable<Project> getProjects() {
 
-		String urlStr = "http://www.agilezen.com/api/v1/projects/";
+		String urlStr = "http://www.agilezen.com/api/v1/projects";
 
 		List<Project> projects = new ArrayList<Project>();
 
 		try {
-			HttpClient client = new HttpClient();
+            URL url = new URL(urlStr);
+            
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            
+            connection.setRequestProperty("X-Zen-ApiKey", "1e6a8c37496144f1ba42a7f86b6f693f");
+            
+            connection.connect();
+            
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
 
-			HttpMethod method = new GetMethod(urlStr);
-
-			method.setRequestHeader("X-Zen-ApiKey", "1e6a8c37496144f1ba42a7f86b6f693f");
-
-			client.executeMethod(method);
-
+            String response = "";
+            
+            while ((line = reader.readLine()) != null) {
+                response += line;
+            }
+            reader.close();
+	                  
 			JAXBContext jc = JAXBContext.newInstance(Projects.class);
 			
 			Unmarshaller unm = jc.createUnmarshaller();
 			
-			String response = method.getResponseBodyAsString();
+			response = response.substring(3);
 			
-			// Skip first byte
-			Projects projectList = (Projects) unm.unmarshal(new StringReader(response.substring(1)));
+			// Skip first 3 bytes
+			Projects projectList = (Projects) unm.unmarshal(new StringReader(response));
 			
 			for(Project project : projectList.getItem().getProjects()){
-				projects.add(project);
+				projects.add(new Project2(project.getName()));
 			}
+			
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			throw e;
+			projects.add(new ProjectImpl("Exception project " + e.getMessage()));
 		}
 
 		return projects;
--- src/com/maventhought/kanbanpro/server/agilezen/ProjectImpl.java
+++ src/com/maventhought/kanbanpro/server/agilezen/ProjectImpl.java
@@ -3,13 +3,20 @@ package com.maventhought.kanbanpro.server.agilezen;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
+import com.google.gwt.user.client.rpc.IsSerializable;
 import com.maventhought.kanbanpro.client.Project;
 
 @XmlType(name="project")
-public class ProjectImpl implements Project {
+public class ProjectImpl implements Project, IsSerializable {
 
 	@XmlElement
 	private String name;
+
+	public ProjectImpl() {}
+	
+	public ProjectImpl(String newName) {
+		this.name = newName;
+	}
 
 	@Override
 	public String getName() {
